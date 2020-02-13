71ff742b560bd788ed883436e77f5af6333fa995
javascriptures <jtjavascripture@gmail.com>
2020-02-13 12:04:35 -0800

    added cors

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const todosController = require('./controllers/todos');
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
 app.use('/todos', todosController);
 
 app.listen(4000, () => {
