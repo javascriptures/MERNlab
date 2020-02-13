4d3d34163c99f690fb8c704800f7aa879a71b801
javascriptures <jtjavascripture@gmail.com>
2020-02-13 09:13:08 -0800

    postman tested api

--- controllers/todos.js
+++ controllers/todos.js
@@ -9,43 +9,40 @@ router.get('/', (req, res) => {
       res.json(todos));
     });
 
-router.get('/todos', (req, res) => {
-  Todo.find({})
-    .then(todos =>
-      res.json(todos))
-});
-
-router.get('/new', (req,res) => {
-  Todo.then(todo => res.json(todo))
+router.get('/:id', (req, res) => {
+    Todo.findById(req.params.id)
+    .then(todo => res.json(todo));
 });
 
 router.get('/:id/edit', (req, res) => {
-  Todo.findById(req.params.id)
-  .then(todo => res.json(todo)
-  );
+    Todo.findById(req.params.id)
+    .then(todo => res.json(todo)
+    );
 });
 
-router.put('/:id', (req, res) => {
-  const complete = req.body.complete === 'on' ? true: false;
-  const todo = {
+router.post('/', (req, res) => {
+    Todo.create(req.body)
+      .then(todo => res.json(todo))
+  });
+
+router.put('/:id/edit', (req, res) => {
+const complete = req.body.complete === 'on' ? true: false;
+const todo = {
     title: req.body.title,
     complete
-  }
-  Todo.findOneAndUpdate({ _id: req.params.id }, todo, { new: true }).then(
+}
+Todo.findOneAndUpdate({ _id: req.params.id }, todo, { new: true }).then(
     todo => 
-      res.json(todo)
-  );
+    res.json(todo)
+);
 });
 
-
-router.get('/:id', (req, res) => {
-  Todo.findById(req.params.id)
-  .then(todo => res.json(todo));
-});
-
-router.post('/todos', (req, res) => {
-  Todo.create(req.body)
-    .then(todo => res.json(todo))
-});
+router.delete("/:id", (req, res) => {
+    // use title as a param to find the record we want to remove
+    Todo.findOneAndDelete({ id: req.params._id }).then(todo => {
+      // send back the deleted document
+      res.json(todo);
+    })
+  });
 
 module.exports = router;
